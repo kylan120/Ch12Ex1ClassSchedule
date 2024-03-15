23ff8a1ad64f0b87f0ae7419bd99d2945326cc6c
kylan120 <142834131+kylan120@users.noreply.github.com>
2024-03-15 09:05:48 -0500

    Did steps 14-21

--- ClassSchedule/Migrations/20240315140329_Schedules.Designer.cs
+++ ClassSchedule/Migrations/20240315140329_Schedules.Designer.cs
@@ -4,6 +4,7 @@ using ClassSchedule.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ch12Ex1ClassSchedule.Migrations
 {
     [DbContext(typeof(ClassScheduleContext))]
-    partial class ClassScheduleContextModelSnapshot : ModelSnapshot
+    [Migration("20240315140329_Schedules")]
+    partial class Schedules
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ClassSchedule/Migrations/20240315140329_Schedules.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ch12Ex1ClassSchedule.Migrations
+{
+    /// <inheritdoc />
+    public partial class Schedules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_Teachers_TeacherId",
+                table: "Classes");
+
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 1,
+                column: "MilitaryTime",
+                value: "1500");
+
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 6,
+                column: "DayId",
+                value: 5);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_Teachers_TeacherId",
+                table: "Classes",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "TeacherId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_Teachers_TeacherId",
+                table: "Classes");
+
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 1,
+                column: "MilitaryTime",
+                value: "1100");
+
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 6,
+                column: "DayId",
+                value: 3);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_Teachers_TeacherId",
+                table: "Classes",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "TeacherId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
